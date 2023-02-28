e148b587d52d36ce383543e83966dfe37cd26743
Dasha Yarkovska <yarkovska11@gmail.com>
2023-02-28 22:32:04 +0200

    edit filtered selector and sort

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,43 +1,26 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { FaRegTrashAlt } from 'react-icons/fa';
 
-import { selectContacts, selectFilter } from 'redux/contacts/selectors';
+import { selectFilteredContacts } from 'redux/contacts/selectors';
 import { deleteContact } from 'redux/contacts/operations';
 
 import { ContactInfo, ContactItem, DeleteBtn } from './ContactList.styled';
 import { Notification } from 'components/common/Notification/Notification.styled';
 
 export const ContactList = () => {
-  const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectFilter);
+  const filteredContacts = useSelector(selectFilteredContacts);
   const dispatch = useDispatch();
-
-  const getFilteredContacts = (contacts, filter) => {
-    const filtered =
-      filter && contacts.length
-        ? contacts.filter(({ name }) =>
-            name.trim().toLowerCase().includes(filter.trim().toLowerCase())
-          )
-        : contacts;
-
-    return filtered;
-    // return (
-    //   Boolean(contacts.length) &&
-    //   filtered.sort((a, b) => a.name.localeCompare(b.name))
-    // );
-  };
 
   const handleClickDelete = id => {
     console.log(id);
     dispatch(deleteContact(id));
   };
 
-  const filtered = getFilteredContacts(contacts, filter);
   return (
     <>
-      {Boolean(filtered.length) ? (
+      {Boolean(filteredContacts.length) ? (
         <ul>
-          {filtered.map(({ name, phone, id }) => (
+          {filteredContacts.map(({ name, phone, id }) => (
             <ContactItem key={id}>
               <div>
                 <ContactInfo>{name}</ContactInfo>
